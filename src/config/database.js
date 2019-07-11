be21da3828a77e59e3a4c8852eaf42d2d906c088
Thiago Correa Lanza Guimarães (SEPLAG) <m752454@ca.intra>
2019-07-11 14:18:08 -0300

    initial

---
+++ src/config/database.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+module.exports = mongoose.connect('mongodb://localhost/mymoney',  { useNewUrlParser: true } )
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
